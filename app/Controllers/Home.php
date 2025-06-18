84fe1d3c93fcccf8ff54cdb4693e089cdc28e460
txc-000 <fafatheking888@gmail.com>
2025-06-19 03:18:40 +0700

    menambahkan api data transaksi pembelian

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,23 +2,65 @@
 
 namespace App\Controllers;
 
-use App\Database\Migrations\Product;
 use App\Models\ProductModel;
+use App\Models\TransactionModel;
+use App\Models\TransactionDetailModel;
 
 class Home extends BaseController
 {
     protected $product;
-    function __construct()
+    protected $transaction;
+    protected $transaction_detail;
+
+    public function __construct()
     {
         helper('form');
         helper('number');
         $this->product = new ProductModel();
+        $this->transaction = new TransactionModel();
+        $this->transaction_detail = new TransactionDetailModel();
     }
-    public function index(): string
+
+    public function index()
     {
         $product = $this->product->findAll();
         $data['product'] = $product;
 
         return view('v_home', $data);
+    }
+
+    public function profile()
+    {
+        $username = session()->get('username');
+        $data['username'] = $username;
+
+        $buy = $this->transaction->where('username', $username)->findAll();
+        $data['buy'] = $buy;
+
+        $product = [];
+
+        if (!empty($buy)) {
+            foreach ($buy as $item) {
+                $detail = $this->transaction_detail->select('transaction_detail.*, product.nama, product.harga, product.foto')->join('product', 'transaction_detail.product_id=product.id')->where('transaction_id', $item['id'])->findAll();
+
+                if (!empty($detail)) {
+                    $product[$item['id']] = $detail;
+                }
+            }
+        }
+
+        $data['product'] = $product;
+
+        return view('v_profile', $data);
+    }
+
+    public function faq()
+    {
+        return view('v_faq');
+    }
+
+    public function contact()
+    {
+        return view('v_contact');
     }
 }
